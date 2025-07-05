51bf5c82310b518310b8ab65d6580572882fb46e
Urban-Elf <mwf.outing710@passinbox.com>
2025-07-05 19:50:47 -0400

    Add copyright headers

--- app/src/main/java/com/urbanelf/iat/WebDriverFactory.java
+++ app/src/main/java/com/urbanelf/iat/WebDriverFactory.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Invision Archive Tools (IAT).
+ *
+ * Copyright (C) 2025 Mark Fisher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
 package com.urbanelf.iat;
 
 import org.openqa.selenium.chrome.ChromeDriver;
