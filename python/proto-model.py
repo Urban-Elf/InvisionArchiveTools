26eb29b9c4bf77f81f371b548c6f72b37d871e0b
Urban-Elf <mwf.outing710@passinbox.com>
2025-07-08 11:46:20 -0400

    Update python module

--- python/proto-model.py
+++
@@ -1,8 +0,0 @@
-import json;
-
-class WorkerData:
-    def __init__():
-        pass
-
-    def from_json(json_str):
-        json_obj = json.loads(json_str)
---
+++ python/worker/ic.py
@@ -0,0 +1,75 @@
+#  This file is part of Invision Archive Tools (IAT).
+#
+#  Copyright (C) 2025 Mark Fisher
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see https://www.gnu.org/licenses/.
+
+import util
+
+class IC:
+    def __init__(self, root_url):
+        self.root_url = root_url
+
+    def auth(self):
+        """Returns a URL to the login page for the community."""
+        raise NotImplementedError()
+
+    def messenger(self):
+        """Returns a URL to the messenger app on the community."""
+        raise NotImplementedError()
+
+    def get_version_number(self):
+        raise NotImplementedError()
+
+    @staticmethod
+    def PHP_data(url: str, data: str):
+        url_suffix = ""
+        if not url.endswith("/"):
+            url_suffix = "/"
+        data_prefix = ""
+        if not data.startswith("?"):
+            data_prefix = "?"
+        return url + url_suffix + data_prefix + data
+
+class IC4(IC):
+    def __init__(self, json_data):
+        super().__init__(json_data)
+
+        self.auth_url = self.root_url + "/login"
+        self.messenger_url = self.root_url + "/messenger"
+
+    def auth(self):
+        return self.auth_url
+    
+    def messenger(self):
+        return self.messenger_url
+
+    def get_version_number(self):
+        return 4
+    
+class IC5(IC):
+    def __init__(self, json_data):
+        super().__init__(json_data)
+
+        self.auth_url = self.root_url + "/login"
+        self.messenger_url = self.root_url + "/messenger"
+
+    def auth(self):
+        return self.auth_url
+    
+    def messenger(self):
+        return self.messenger_url
+
+    def get_version_number(self):
+        return 5
