162fa4b0efe33d6206b626d6f9e6c7db74f5c937
Urban-Elf <UrbanElf34@proton.me>
2025-09-27 14:38:53 -0400

    Fix issue with bootstrap spawning runaway processes on Mac

--- server/bootstrap.py
+++ server/bootstrap.py
@@ -1,6 +1,9 @@
+import multiprocessing
 import sys
-import bs4 # Kind of a hack to ensure PyInstaller sees this lib
+import bs4  # ensure PyInstaller sees this
 from src.main import main
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    multiprocessing.set_start_method("spawn", force=True)
     main(sys.argv)
